a295d66fa623528a80ff643687e69a6131e1fedb
Daniel Tombor <t.dani12@gmail.com>
2026-02-13 16:53:40 +0000

    Initial Commit

---
+++ runimation/runimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  runimationApp.swift
+//  runimation
+//
+//  Created by Daniel Tombor on 13/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct runimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ runimationTests/runimationTests.swift
@@ -0,0 +1,16 @@
+//
+//  runimationTests.swift
+//  runimationTests
+//
+//  Created by Daniel Tombor on 13/02/2026.
+//
+
+import Testing
+
+struct runimationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
